834459593a2bd3d7adffe331a047b2deaf41c923
paolomazzoleni4 <84578580+paolomazzoleni4@users.noreply.github.com>
2025-03-04 21:30:17 +0100

    Add files via upload
    Aggiornamento file di codice principale. Funzionante, scrive 25.000 dati letti da microfono mems su una micro SD

--- code/audio_on_file.ino
+++ code/audio_on_file.ino
@@ -7,8 +7,9 @@ const int micPin  = A0;
 //Variables
 int micVal  = 0;
 int i = 0;
+//int j = 0;
 File myFile;
-int valori [5000];
+int valori [25000];
 
 void setup() {
   //inizializzo, imposto il pin output, attendo la seriale
@@ -19,7 +20,6 @@ void setup() {
 
   //controllo comunicazione con SD
   if (!SD.begin(10)) {
-    // se non a buon fine, errore e rimango in loop
     Serial.println("errore di inizializzazione");
     while(1);
   }
@@ -29,20 +29,32 @@ void loop() {
   //leggo valore e lo salvo in posizione i array
   micVal = analogRead(micPin);
   valori[i] = micVal;
-  i++;
+  i++; //contatore parziale
+  //j++; //contatore totale
 
-  //quando arrivo a fine lettura 5000 valori, devo scriverli su sd
-  if(i >= 5000){
-    myFile = SD.open("test.txt", FILE_WRITE);
+  //quando arrivo a fine lettura 25000 valori, devo scriverli su sd
+  if(i >= 25000){
+    Serial.println("inizio scrittura dati");
+    myFile = SD.open("dati.txt", FILE_WRITE);
     if (myFile) {
-      for(i=0;i<5000;i++){
+      for(i=0;i<25000;i++){
         myFile.println(valori[i]);
       }
-      Serial.print("scrittura 5.000 dati avvenuta");
+      Serial.println("scrittura 25.000 dati avvenuta");
     } else {
-      Serial.print("errore nell'apertura del file");
+      Serial.println("errore nell'apertura del file");
     }
     myFile.close();
+    i = 0;
+    Serial.println("fine scrittura dati");
     while(1);
+    /*
+    //scrivo 10 volte 25.000 valori
+    //a 250.000 valori finisce il programma
+    if(j >= 250000){
+      Serial.println("fine TUTTO");
+      while(1);
+    }
+    */
   }
 }
